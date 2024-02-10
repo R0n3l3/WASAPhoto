116db9b288b9838dea94b512d8025dfcabeb724b
R0n3l3 <ronca.1983209@studenti.uniroma1.it>
2024-02-10 17:29:49 +0100

    fixed errors

--- cmd/webapi/cors.go
+++ cmd/webapi/cors.go
@@ -11,7 +11,7 @@ import (
 func applyCORSHandler(h http.Handler) http.Handler {
 	return handlers.CORS(
 		handlers.AllowedHeaders([]string{
-			"x-example-header",
+			"content-type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "X-Requested-With", "Authorization",
 		}),
 		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"}),
 		// Do not modify the CORS origin and max age, they are used in the evaluation.
--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -35,22 +35,22 @@ export default {
     }
   },
   mounted() {
-}
+  }
 }
 </script>
 
 <template>
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <h1 class="h2">Welcome to WASAPhoto</h1>
+    <h1 class="h2">Welcome to WASAPhoto</h1>
+  </div>
+  <div class="input-group mb-3">
+    <input type="text" id="username" v-model="username" class="form-control" placeholder="Insert a username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+    <div class="input-group-append">
+      <button class="btn btn-success" type="button" @click="doLogin">Login</button>
     </div>
-    <div class="input-group mb-3">
-      <input type="text" id="username" v-model="username" class="form-control" placeholder="Insert a username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-      <div class="input-group-append">
-        <button class="btn btn-success" type="button" @click="doLogin">Login</button>
-      </div>
-    </div>
+  </div>
 
-    <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
+  <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
 </template>
 
 <style>
